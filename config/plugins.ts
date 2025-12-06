faa03bd236c802feadd3c5ef46609cbae67bc119
Ledio Nase <ledio.nase@gmail.com>
2025-12-06 10:11:12 +0100

    Cloudinary test

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,19 @@
-export default () => ({
+export default ({ env }) => ({
   'cloud-cronjob-runner': {
     enabled: false,
+  },
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
   },
 });
