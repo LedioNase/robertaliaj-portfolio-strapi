4dc33fa44bc95663315e4bbd27257c26421c13ee
Ledio Nase <ledio.nase@gmail.com>
2025-12-06 16:09:50 +0100

    test without Cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,35 +1,22 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
   {
-    name: 'strapi::security',
+    name: "strapi::body",
     config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  {
-    name: 'strapi::body',
-    config: {
-      formLimit: '512mb',    // Form body size limit
-      jsonLimit: '512mb',    // JSON body size limit
-      textLimit: '512mb',    // Text body size limit
+      formLimit: "512mb", // Form body size limit
+      jsonLimit: "512mb", // JSON body size limit
+      textLimit: "512mb", // Text body size limit
       formidable: {
         maxFileSize: 512 * 1024 * 1024, // 512MB in bytes
       },
     },
   },
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,5 @@
-export default ({ env }) => ({
+export default () => ({
   'cloud-cronjob-runner': {
     enabled: false,
-  },
-  upload: {
-    config: {
-      provider: '@strapi/provider-upload-cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
   },
 });
