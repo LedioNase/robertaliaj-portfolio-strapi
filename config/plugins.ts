8f82fed0b3e10b560ff6d5563da187326fe43ce6
Ledio Nase <ledio.nase@gmail.com>
2025-12-12 09:46:48 +0100

    Changed to allowedTypes in config/plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,18 +6,7 @@ export default ({ env }) => ({
     config: {
       sizeLimit: 512 * 1024 * 1024,
       security: {
-        allowedMimeTypes: [
-          'image/jpeg',
-          'image/png',
-          'image/gif',
-          'image/webp',
-          'image/svg+xml',
-          'video/mp4',
-          'audio/mpeg',
-          'application/pdf',
-          'text/plain',
-        ],
-        allowedExtensions: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg', '.mp4', '.mp3', '.pdf', '.txt'],
+        allowedTypes: ['image/*', 'video/*', 'audio/*', 'application/pdf', 'text/*'],
       },
     },
   },
