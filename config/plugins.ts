2a5a2ca800056842cda07ecd723517f81d016d0f
Ledio Nase <ledio.nase@gmail.com>
2025-12-12 08:25:10 +0100

    Added upload security config

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,4 +2,12 @@ export default () => ({
   'cloud-cronjob-runner': {
     enabled: false,
   },
+  upload: {
+    config: {
+      security: {
+        enabled: true,
+        fileTypes: ['image', 'video', 'audio', 'pdf', 'text'],
+      },
+    },
+  },
 });
